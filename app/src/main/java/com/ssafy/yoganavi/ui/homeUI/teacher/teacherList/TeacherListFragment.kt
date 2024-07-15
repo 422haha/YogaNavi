ed7cdab3b9ddbb929c6dd1ba5ebaf19278529ffc
yeolife <yeo2507@naver.com>
2024-07-15 16:01:05 +0900

    S11P12D210-22 Feat: 강사 리스트 UI 완성

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherList/TeacherListFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherList/TeacherListFragment.kt
@@ -1,9 +1,27 @@
 package com.ssafy.yoganavi.ui.homeUI.teacher.teacherList
 
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.yoganavi.databinding.FragmentTeacherListBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
 
 class TeacherListFragment :
     BaseFragment<FragmentTeacherListBinding>(FragmentTeacherListBinding::inflate) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding.rvTeacherList){
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+
+            // 구분선
+            val dividerItemDecoration = DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
 }
