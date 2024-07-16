8bfc7acae8e7b7e604e074494b008324175d9c0a
yeolife <yeo2507@naver.com>
2024-07-16 10:53:07 +0900

    S11P12D210-23 feat: 1. FilterFragment에 material timepicker 띄우는 함수 생성 2. Filter 라디오버튼 글자 크기 키움

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/filter/FilterFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/filter/FilterFragment.kt
@@ -1,8 +1,33 @@
 package com.ssafy.yoganavi.ui.homeUI.teacher.filter
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.ssafy.yoganavi.databinding.FragmentFilterBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
 
 class FilterFragment : BaseFragment<FragmentFilterBinding>(FragmentFilterBinding::inflate) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun showTimePicker() {
+        val materialTimePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setInputMode(MaterialTimePicker.INPUT_MODE_KEYBOARD)
+            .setTitleText("시작 시간")
+            .setNegativeButtonText("취소")
+            .setPositiveButtonText("확인")
+            .setTheme(android.R.style.Theme_Black)
+            .build()
+
+        materialTimePicker.addOnPositiveButtonClickListener {
+            materialTimePicker.hour
+            materialTimePicker.minute
+        }
+
+        materialTimePicker.show(requireActivity().supportFragmentManager, "fragment_tag")
+    }
 }
