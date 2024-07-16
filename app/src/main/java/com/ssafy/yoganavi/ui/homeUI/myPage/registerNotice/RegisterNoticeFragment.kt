481aceaaaf35eb1d62cffa2e92445881b97642e5
정다혜 <hyejjang1203@naver.com>
2024-07-16 10:05:33 +0900

    S11P12D210-29 feat 공지사항 작성 view extra 레이아웃 삭제

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerNotice/RegisterNoticeFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerNotice/RegisterNoticeFragment.kt
@@ -1,9 +1,24 @@
 package com.ssafy.yoganavi.ui.homeUI.myPage.registerNotice
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.ssafy.yoganavi.databinding.FragmentRegisterNoticeBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class RegisterNoticeFragment :
     BaseFragment<FragmentRegisterNoticeBinding>(FragmentRegisterNoticeBinding::inflate) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(2000)
+            binding.ivPhoto.visibility = View.VISIBLE
+            binding.ivCancel2.visibility = View.VISIBLE
+        }
+    }
 }
