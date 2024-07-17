18d6c6232729cff36c5158340a4caf832ff1e851
yeolife <yeo2507@naver.com>
2024-07-17 15:25:41 +0900

    S11P12D210-18-3-5 feat: main 임시 네비게이션

--- app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.ssafy.yoganavi.ui.core
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.yoganavi.R
+import com.ssafy.yoganavi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnv
+            .setupWithNavController(navController)
     }
 }
