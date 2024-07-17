a71e281ed4cd1f91de37370045c38303259775f6
강미연 <422haha@naver.com>
2024-07-17 17:35:21 +0900

    feat: [BE] ArticleRepository 인터페이스 추가

---
+++ backend/src/main/java/com/yoga/backend/article/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.yoga.backend.article;
+
+import com.yoga.backend.common.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
