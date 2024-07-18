ca9b9dd24077b3832c1f9b8b15d8e7c1f9fd7d12
yeolife <yeo2507@naver.com>
2024-07-18 12:37:44 +0900

    S11P12D210-18-3-5 feat: 1. 리스트 빈 화면 만듦 2. 바텀 네비 아래 빈공간 색칠 3. 바텀네비와 키보드 같이 올라오는 버그 수정 4. searchbar 전체  선택

--- app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
@@ -6,19 +6,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -26,9 +27,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        initGestureBarColor()
+
+        connectBottomNav()
+    }
+
+    private fun connectBottomNav() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnv
             .setupWithNavController(navController)
+    }
+
+
+    private fun initGestureBarColor() {
+        // 시스템 제스처 바 색상 설정
+        window.navigationBarColor = resources.getColor(R.color.bottomnav)
+
+        // Edge-to-Edge 설정
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
     }
 }
