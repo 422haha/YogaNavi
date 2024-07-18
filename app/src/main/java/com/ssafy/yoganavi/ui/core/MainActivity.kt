e70389c9811d6262e6b4a6e4cd76cea334684068
Aloe-droid <csjune99@naver.com>
2024-07-18 15:54:58 +0900

    S11P12D210-45 Fix: 다시 보내기

--- app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
@@ -2,10 +2,7 @@ package com.ssafy.yoganavi.ui.core
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -19,17 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initGestureBarColor()
-
         connectBottomNav()
     }
 
