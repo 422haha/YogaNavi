e9947b0b80a076950b7662da564cc85ce620dfe4
Aloe-droid <https://github.com/Aloe-droid>
2024-07-18 22:54:10 +0900

    S11P12D210-86 Feat: 영상 강의 목록 -> 상세 강의로 화면 변경

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerVideo/RegisterVideoFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerVideo/RegisterVideoFragment.kt
@@ -1,9 +1,18 @@
 package com.ssafy.yoganavi.ui.homeUI.myPage.registerVideo
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.ssafy.yoganavi.databinding.FragmentRegisterVideoBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
 
-class RegisterVideoFragment :
-    BaseFragment<FragmentRegisterVideoBinding>(FragmentRegisterVideoBinding::inflate) {
+class RegisterVideoFragment : BaseFragment<FragmentRegisterVideoBinding>(
+    FragmentRegisterVideoBinding::inflate
+) {
+    private val args by navArgs<RegisterVideoFragmentArgs>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        println(args.recordedId)
+    }
 }
