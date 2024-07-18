a3dec0603c54bca233fa4e62460f79c3410d5545
Aloe-droid <csjune99@naver.com>
2024-07-18 15:49:17 +0900

    S11P12D210-45 Fix: 테스트 코드 제거

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/schedule/home/HomeFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/schedule/home/HomeFragment.kt
@@ -1,26 +1,9 @@
 package com.ssafy.yoganavi.ui.homeUI.schedule.home
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.ssafy.yoganavi.databinding.FragmentHomeBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-    private val viewModel: HomeViewModel by viewModels()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.btn.setOnClickListener {
-            viewLifecycleOwner.lifecycleScope.launch {
-                viewModel.test(str = "test 입니다")
-            }
-        }
-
-    }
 }
