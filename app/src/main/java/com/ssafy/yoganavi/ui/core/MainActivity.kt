be9bca03ce483f5daf31c7205d2bdf0f9a98c2c7
yeolife <yeo2507@naver.com>
2024-07-18 13:57:34 +0900

    S11P12D210-18-3-5 feat: 1. 상태바 색깔 변경 2. S10 바텀네비 오류

--- app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
@@ -1,31 +1,26 @@
 package com.ssafy.yoganavi.ui.core
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initGestureBarColor()
 
@@ -39,16 +34,16 @@ class MainActivity : AppCompatActivity() {
             .setupWithNavController(navController)
     }
 
-
     private fun initGestureBarColor() {
         // 시스템 제스처 바 색상 설정
         window.navigationBarColor = resources.getColor(R.color.bottomnav)
 
-        // Edge-to-Edge 설정
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                )
+        // 상단 상태바 색상 변경
+        window.apply {
+
+            statusBarColor = Color.WHITE
+            //상태바 아이콘(true: 검정 / false: 흰색)
+            WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = true
+        }
     }
 }
