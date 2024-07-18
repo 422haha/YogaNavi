d33460dfb2178a8a6cf2693e0ce545f31bd03f9c
강미연 <422haha@naver.com>
2024-07-18 15:25:46 +0900

    S11P12D210-40 <feat> [be]: ArticleDto 추가

--- backend/src/main/java/com/yoga/backend/article/ArticleDto.java
+++ backend/src/main/java/com/yoga/backend/article/ArticleDto.java
@@ -2,9 +2,12 @@ package com.yoga.backend.article;
 
 import lombok.Data;
 
+/**
+ * 게시글(공지사항) DTO 클래스
+ */
 @Data
 public class ArticleDto {
-    private String title;
-    private String content;
-    private String imageUrl;
+    private String title; // 게시글 제목
+    private String content; // 게시글 내용
+    private String imageUrl; // 이미지 URL
 }
