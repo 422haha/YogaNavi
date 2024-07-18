50fc418565fa8ac75c69915ee0d5a27edace2c6c
yeolife <yeo2507@naver.com>
2024-07-18 14:04:33 +0900

    S11P12D210-18-3-5 feat: 색깔 가져오는 디플리케이트 제거

--- app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/MainActivity.kt
@@ -36,11 +36,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun initGestureBarColor() {
         // 시스템 제스처 바 색상 설정
-        window.navigationBarColor = resources.getColor(R.color.bottomnav)
+        window.navigationBarColor = resources.getColor(R.color.bottomnav, null)
 
         // 상단 상태바 색상 변경
         window.apply {
-
             statusBarColor = Color.WHITE
             //상태바 아이콘(true: 검정 / false: 흰색)
             WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = true
