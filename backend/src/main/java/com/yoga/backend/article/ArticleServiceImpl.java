1979671783ea5962fedd774835b9810789955fdc
강미연 <422haha@naver.com>
2024-07-18 15:26:30 +0900

    S11P12D210-40 <feat> [be]: ArticleServiceImpl 구현

--- backend/src/main/java/com/yoga/backend/article/ArticleServiceImpl.java
+++ backend/src/main/java/com/yoga/backend/article/ArticleServiceImpl.java
@@ -5,20 +5,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * 게시글(공지사항) 서비스 구현 클래스
+ */
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
-    @Autowired
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
+    @Autowired
+    public ArticleServiceImpl(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    /**
+     * 게시글을 저장합니다.
+     *
+     * @param article 저장할 게시글
+     */
     @Override
     public void saveArticle(Article article) {
         articleRepository.save(article);
     }
 
+    /**
+     * 모든 게시글을 조회합니다.
+     *
+     * @return 게시글 목록
+     */
     @Override
     public List<Article> getAllArticles() {
         return articleRepository.findAll();
+    }
+
+    /**
+     * 특정 사용자가 작성한 게시글을 조회합니다.
+     *
+     * @param userId 사용자 ID
+     * @return 게시글 목록
+     */
+    @Override
+    public List<Article> getArticlesByUserId(int userId) { // userId 타입을 int로 수정
+        return articleRepository.findByUserId(userId);
+    }
+
+    /**
+     * 게시글 ID로 특정 게시글을 조회합니다.
+     *
+     * @param id 게시글 ID
+     * @return 게시글
+     */
+    @Override
+    public Optional<Article> getArticleById(Long id) {
+        return articleRepository.findById(id);
+    }
+
+    /**
+     * 게시글을 삭제합니다.
+     *
+     * @param id 삭제할 게시글 ID
+     */
+    @Override
+    public void deleteArticle(Long id) {
+        articleRepository.deleteById(id);
     }
 }
