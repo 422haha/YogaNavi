e7b94837d053b3f65b2446be753730fc6de741b8
정다혜 <hyejjang1203@naver.com>
2024-07-19 17:49:25 +0900

    S11P12D210-79 Feat : 공지사항 list retrofit연결

--- app/src/main/java/com/ssafy/yoganavi/data/repository/InfoRepository.kt
+++ app/src/main/java/com/ssafy/yoganavi/data/repository/InfoRepository.kt
@@ -2,10 +2,13 @@ package com.ssafy.yoganavi.data.repository
 
 import com.ssafy.yoganavi.data.source.lecture.LectureData
 import com.ssafy.yoganavi.data.source.lecture.LectureDetailData
+import com.ssafy.yoganavi.data.source.notice.NoticeData
 
 interface InfoRepository {
 
     suspend fun getLectureList(): ListResponse<LectureData>
 
     suspend fun getLecture(recordId: Int): DetailResponse<LectureDetailData>
+
+    suspend fun getNoticeList() : ListResponse<NoticeData>
 }
--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/notice/NoticeFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/notice/NoticeFragment.kt
@@ -1,27 +1,35 @@
 package com.ssafy.yoganavi.ui.homeUI.myPage.notice
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.navigation.Navigation.findNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.databinding.FragmentNoticeBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
+import com.ssafy.yoganavi.ui.homeUI.myPage.managementVideo.ManagementVideoFragmentDirections
+import com.ssafy.yoganavi.ui.homeUI.myPage.notice.notices.NoticeAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class NoticeFragment : BaseFragment<FragmentNoticeBinding>(FragmentNoticeBinding::inflate) {
+
+    private val viewModel : NoticeViewModel by viewModels()
+    private val noticeAdapter by lazy{ NoticeAdapter(::navigateToNoticeFragment) }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity()
+
+        binding.rvMyList.adapter = noticeAdapter
+        initCollect()
         initListener()
-        initRecyclerView()
+        viewModel.getNoticeAll()
     }
 
     private fun initListener() {
@@ -31,62 +39,18 @@ class NoticeFragment : BaseFragment<FragmentNoticeBinding>(FragmentNoticeBinding
             }
         }
     }
-    private fun initRecyclerView(){
-        val itemList = java.util.ArrayList<NoticeItem>()
-        itemList.add(NoticeItem("https://cdn.inflearn.com/public/files/posts/657d0bce-45a2-4977-9520-4490639c95b6/image.png","요구르드","2022-02-03","늦지마세요~"))
-        itemList.add(NoticeItem("https://cdn.inflearn.com/public/files/posts/657d0bce-45a2-4977-9520-4490639c95b6/image.png","요구르드","2022-02-03","늦지마세요~"))
-        itemList.add(NoticeItem("https://cdn.inflearn.com/public/files/posts/657d0bce-45a2-4977-9520-4490639c95b6/image.png","요구르드","2022-02-03","늦지마세요~"))
-        itemList.add(NoticeItem("https://cdn.inflearn.com/public/files/posts/657d0bce-45a2-4977-9520-4490639c95b6/image.png","요구르드","2022-02-03","늦지마세요~"))
-        val noticeAdapter = NoticeAdapter(itemList)
-        noticeAdapter.notifyDataSetChanged()
-
-        binding.rvMyList.adapter = noticeAdapter
-        binding.rvMyList.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
-    }
-}
-
-class NoticeAdapter(val itemList:ArrayList<NoticeItem>) : RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>(){
-    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int): NoticeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_notice,parent,false)
-        return NoticeViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: NoticeViewHolder, position: Int) {
-        holder.bindItems(itemList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return itemList.count()
-    }
-    inner class NoticeViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
-        fun bindItems(item:NoticeItem){
-            val ivProfile = itemView.findViewById<ImageView>(R.id.iv_profile)
-            val tvTeacherNickname = itemView.findViewById<TextView>(R.id.tv_teacher_nickname)
-            val tvDate = itemView.findViewById<TextView>(R.id.tv_date)
-            val tvEditBtn = itemView.findViewById<TextView>(R.id.tv_editBtn)
-            val tvDeleteBtn = itemView.findViewById<TextView>(R.id.tv_deleteBtn)
-            val tvContent = itemView.findViewById<TextView>(R.id.tv_content)
-
-            Glide.with(itemView.context)
-                .load(item.ivProfile)
-                .circleCrop()
-                .into(ivProfile)
-            tvTeacherNickname.text = item.tvTeacherNickname
-            tvDate.text = item.tvDate
-            tvContent.text = item.tvContent
-            tvEditBtn.setOnClickListener {
-                findNavController(itemView).navigate(R.id.action_noticeFragment_to_registerNoticeFragment)
-            }
-            tvDeleteBtn.setOnClickListener {
-                // TODO: delete구현
+    private fun initCollect() = viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.noticeList.collectLatest {
+                noticeAdapter.submitList(it)
             }
         }
     }
-}
 
-data class NoticeItem(
-    val ivProfile: String,
-    val tvTeacherNickname: String,
-    val tvDate: String,
-    val tvContent: String
-)
+    private fun navigateToNoticeFragment(recordedId: Int = -1) {
+        val directions = ManagementVideoFragmentDirections
+            .actionManagementVideoFragmentToRegisterVideoFragment(recordedId)
+
+        findNavController().navigate(directions)
+    }
+}
