610a5923bf25884ef5f6f51371f81f3b6b8b5be5
yeolife <yeo2507@naver.com>
2024-07-22 17:23:30 +0900

    S11P12D210-82 feat: 1. 화상강의 DTO 수정 2. 화상강의 등록에 최대 수강인원 추가 3. 화상강의 툴바 추가 4. viewmodel 일부 완성

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerLive/RegisterLiveFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerLive/RegisterLiveFragment.kt
@@ -2,8 +2,42 @@ package com.ssafy.yoganavi.ui.homeUI.myPage.registerLive
 
 import com.ssafy.yoganavi.databinding.FragmentRegisterLiveBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
+import com.ssafy.yoganavi.ui.core.MainEvent
+import com.ssafy.yoganavi.ui.core.MainViewModel
+import com.ssafy.yoganavi.ui.utils.MODIFY_LIVE
+import com.ssafy.yoganavi.ui.utils.REGISTER
+import com.ssafy.yoganavi.ui.utils.REGISTER_LIVE
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterLiveFragment :
     BaseFragment<FragmentRegisterLiveBinding>(FragmentRegisterLiveBinding::inflate) {
 
+    private val args: RegisterLiveFragmentArgs by navArgs()
+
+    private val activityViewModel: MainViewModel by activityViewModels()
+
+    private val viewModel: RegisterLiveViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(args.liveId != -1) {
+            viewModel.getLive(args.liveId)
+        }
+
+        setToolbar(args.liveId)
+    }
+
+
+    private fun setToolbar(liveId: Int) {
+        val mainEvent = MainEvent(
+            isBottomNavigationVisible = false,
+            title = if(liveId == -1) REGISTER_LIVE else MODIFY_LIVE,
+            canGoBack = true,
+            menuItem = REGISTER,
+            menuListener = { viewModel.createLive() }
+        )
+        activityViewModel.setMainEvent(mainEvent)
+    }
 }
--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerLive/RegisterLiveViewModel.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/registerLive/RegisterLiveViewModel.kt
@@ -1,6 +1,34 @@
 package com.ssafy.yoganavi.ui.homeUI.myPage.registerLive
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.yoganavi.data.repository.InfoRepository
+import com.ssafy.yoganavi.data.source.live.LiveLectureData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RegisterLiveViewModel : ViewModel() {
+@HiltViewModel
+class RegisterLiveViewModel @Inject constructor(
+    private val infoRepository: InfoRepository
+) : ViewModel() {
+
+    private val _liveState = MutableStateFlow(LiveLectureData())
+    val liveState: StateFlow<LiveLectureData> = _liveState.asStateFlow()
+
+    fun getLive(liveId: Int) = viewModelScope.launch(Dispatchers.IO) {
+        runCatching { infoRepository.getLive(liveId) }
+            .onSuccess { it.data?.let { lecture -> _liveState.emit(lecture) } }
+            .onFailure { it.printStackTrace() }
+    }
+
+    fun createLive() = viewModelScope.launch(Dispatchers.IO) {
+        runCatching { infoRepository.createLive() }
+            .onSuccess {  }
+            .onFailure { it.printStackTrace() }
+    }
 }
