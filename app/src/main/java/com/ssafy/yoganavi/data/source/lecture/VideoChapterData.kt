b74504a29e2f4918adef03776f6f99a4182eb559
Aloe-droid <csjune99@naver.com>
2024-07-25 17:53:33 +0900

    S11P12D210-144 Feat : DTO 수정

--- app/src/main/java/com/ssafy/yoganavi/data/source/lecture/VideoChapterData.kt
+++ app/src/main/java/com/ssafy/yoganavi/data/source/lecture/VideoChapterData.kt
@@ -1,14 +1,10 @@
 package com.ssafy.yoganavi.data.source.lecture
 
-import java.util.UUID
-
 data class VideoChapterData(
+    val id: Long? = null,
     val chapterTitle: String = "",
     val chapterDescription: String = "",
     val recordVideo: String = "",
-
-    @Transient
-    val id: UUID = UUID.randomUUID(),
 
     @Transient
     val recordPath: String = "",
