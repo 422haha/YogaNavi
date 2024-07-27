714e5ceef677add42f34e5d52187966e955b4b74
서장원 <seojang980510@naver.com>
2024-07-27 16:58:26 +0900

    <feat> [be] 공지사항에 작성자 프로필, 저용량 이미지 전송

--- backend/src/main/java/com/yoga/backend/mypage/article/ArticleDto.java
+++ backend/src/main/java/com/yoga/backend/mypage/article/ArticleDto.java
@@ -8,8 +8,14 @@ import lombok.Data;
 @Data
 public class ArticleDto {
 
-    private String content; // 게시글 내용
-    private String imageUrl; // 이미지 URL
+    private Long articleId;
+    private String content;
+    private String imageUrl;
     private String imageUrlSmall;
+    private String userName;
+    private String profileImageUrl;
+    private String profileImageSmallUrl;
+    private Long createdAt;
+    private Long updatedAt;
 
 }
