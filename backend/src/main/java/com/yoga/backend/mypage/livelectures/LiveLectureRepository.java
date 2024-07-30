fa497c5b665cbb73bff9bd025a13d400e477ea1c
김은섭 <subway@kumoh.ac.kr>
2024-07-30 16:45:45 +0900

    <fix> [be]: LiveLectureRepository에서 join fetch로 수정

--- backend/src/main/java/com/yoga/backend/mypage/livelectures/LiveLectureRepository.java
+++ backend/src/main/java/com/yoga/backend/mypage/livelectures/LiveLectureRepository.java
@@ -3,6 +3,8 @@ package com.yoga.backend.mypage.livelectures;
 import com.yoga.backend.common.entity.LiveLectures;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * 실시간 강의 리포지토리 인터페이스
  * 데이터베이스와의 상호작용을 정의
@@ -14,6 +16,7 @@ public interface LiveLectureRepository extends JpaRepository<LiveLectures, Integ
      * @param id 사용자 ID
      * @return 사용자 ID에 해당하는 화상 강의 목록
      */
+    @Query("SELECT ll FROM LiveLectures ll JOIN FETCH ll.user WHERE ll.user.id = :id")
     List<LiveLectures> findByUserId(Integer id);
 
 }
