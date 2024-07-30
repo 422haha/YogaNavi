fafa616cdc555599da0cc4ef9706052b80f45900
yeolife <yeo2507@naver.com>
2024-07-30 10:28:33 +0900

    S11P12D210-181 feat: Home API 완성

---
+++ app/src/main/java/com/ssafy/yoganavi/data/source/dto/home/HomeData.kt
@@ -0,0 +1,23 @@
+package com.ssafy.yoganavi.data.source.dto.home
+
+import com.google.gson.annotations.SerializedName
+
+data class HomeData(
+    val liveId:Int = 0,
+    @SerializedName("userId")
+    val teacherId: Int = 0,
+    @SerializedName("nickname")
+    val teacherName: String = "",
+    @SerializedName("profileImageUrlSmall")
+    val teacherSmallProfile: String?,
+    @SerializedName("profileImageUrl")
+    val teacherProfile: String?,
+    var liveTitle: String = "",
+    var liveContent : String = "",
+    var lectureDay: String = "",
+    var lectureDate: Long = 0L,
+    var startTime: Long = 0L,
+    var endTime: Long = 0L,
+    var regDate: Long = 0L,
+    var maxLiveNum: Int = 0
+)
