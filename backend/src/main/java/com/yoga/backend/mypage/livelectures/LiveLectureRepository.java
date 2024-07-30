ed7abf0b62925e0caa47a93f53cafbacbfed76eb
김은섭 <subway@kumoh.ac.kr>
2024-07-30 20:27:13 +0900

    <fix> [be]: 컨플릭트 복구

--- backend/src/main/java/com/yoga/backend/mypage/livelectures/LiveLectureRepository.java
+++ backend/src/main/java/com/yoga/backend/mypage/livelectures/LiveLectureRepository.java
@@ -4,9 +4,11 @@ import com.yoga.backend.common.entity.LiveLectures;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LiveLectureRepository extends JpaRepository<LiveLectures, Long> {
+    
+    @Query("SELECT ll FROM LiveLectures ll JOIN FETCH ll.user WHERE ll.user.id = :id")
     List<LiveLectures> findByUserId(int id);
 }
