f6a5f9dd54497f7546817d2f73fcc56a287644c8
yeolife <yeo2507@naver.com>
2024-07-30 22:17:09 +0900

    S11P12D210-174-2-FCM foreground 완성

--- app/src/main/java/com/ssafy/yoganavi/ui/core/LiveFcmService.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/LiveFcmService.kt
@@ -1,5 +1,6 @@
 package com.ssafy.yoganavi.ui.core
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -82,37 +83,15 @@ class LiveFcmService: FirebaseMessagingService() {
         val body = remoteMessage.notification?.body ?: remoteMessage.data["body"]
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
-            .setContentTitle(title) // 제목
-            .setContentText(body) // 메시지 내용
-            .setAutoCancel(true) // 알람 클릭시 삭제여부
-            .setSound(soundUri) // 알림 소리
-            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
-            .setPriority(NotificationCompat.PRIORITY_MAX) // 중요도 (HIGH: 상단바 표시 가능)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
 
         notificationManager.notify(uniId, notificationBuilder.build())
     }
 }
-
-/**
-"notificationBuilder" 알림 생성시 여러가지 옵션을 이용해 커스텀 가능.
-setSmallIcon : 작은 아이콘 (필수)
-setContentTitle : 제목 (필수)
-setContentText : 내용 (필수)
-setColor : 알림내 앱 이름 색
-setWhen : 받은 시간 커스텀 ( 기본 시스템에서 제공합니다 )
-setShowWhen : 알림 수신 시간 ( default 값은 true, false시 숨길 수 있습니다 )
-setOnlyAlertOnce : 알림 1회 수신 ( 동일 아이디의 알림을 처음 받았을때만 알린다, 상태바에 알림이 잔존하면 무음 )
-setContentTitle : 제목
-setContentText : 내용
-setFullScreenIntent : 긴급 알림 ( 자세한 설명은 아래에서 설명합니다 )
-setTimeoutAfter : 알림 자동 사라지기 ( 지정한 시간 후 수신된 알림이 사라집니다 )
-setContentIntent : 알림 클릭시 이벤트 ( 지정하지 않으면 클릭했을때 아무 반응이 없고 setAutoCancel 또한 작동하지 않는다 )
-setLargeIcon : 큰 아이콘 ( mipmap 에 있는 아이콘이 아닌 drawable 폴더에 있는 아이콘을 사용해야 합니다. )
-setAutoCancel : 알림 클릭시 삭제 여부 ( true = 클릭시 삭제 , false = 클릭시 미삭제 )
-setPriority : 알림의 중요도를 설정 ( 중요도에 따라 head up 알림으로 설정할 수 있는데 자세한 내용은 밑에서 설명하겠습니다. )
-setVisibility : 잠금 화면내 알림 노출 여부
-Notification.VISIBILITY_PRIVATE : 알림의 기본 정보만 노출 (제목, 타이틀 등등)
-Notification.VISIBILITY_PUBLIC : 알림의 모든 정보 노출
-Notification.VISIBILITY_SECRET : 알림의 모든 정보 비노출
- */
