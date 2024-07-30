859f2d942c7696e83c740f60df44a79e169353f8
김은섭 <subway@kumoh.ac.kr>
2024-07-30 20:14:01 +0900

    <fix> [be]: 컨플릭트 복구 중

--- backend/src/main/java/com/yoga/backend/home/HomeService.java
+++ backend/src/main/java/com/yoga/backend/home/HomeService.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
