5951cd77e0cee1b181b9cd10704309702667817d
정다혜 <hyejjang1203@naver.com>
2024-07-31 17:46:38 +0900

    S11P12D210-225 Feat: teacher detail 리사이클러뷰로 구현

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherDetail/TeacherDetailViewModel.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherDetail/TeacherDetailViewModel.kt
@@ -3,7 +3,7 @@ package com.ssafy.yoganavi.ui.homeUI.teacher.teacherDetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.yoganavi.data.repository.InfoRepository
-import com.ssafy.yoganavi.data.source.dto.teacher.TeacherData
+import com.ssafy.yoganavi.data.source.dto.teacher.TeacherDetailData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,11 +11,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TeacherDetailViewModel @Inject constructor(
-    val infoRepository: InfoRepository
+    private val infoRepository: InfoRepository
 ) : ViewModel() {
     fun getTeacherDetail(
         teacherId: Int,
-        bindData: suspend (TeacherData) -> Unit
+        bindData: suspend (TeacherDetailData) -> Unit
     ) = viewModelScope.launch(Dispatchers.IO) {
         runCatching { infoRepository.getTeacherDetail(teacherId) }
             .onSuccess { it.data?.let { data -> bindData(data) } }
