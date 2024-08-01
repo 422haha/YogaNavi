904ff54460498bcc364396c1aaa9f23a8f6d9f3d
강미연 <422haha@naver.com>
2024-08-01 22:37:09 +0900

    S11P12D210-264 <chore> [be]: ReservationRepository 수정 – 예약 관련 쿼리 및 메서드 수정

--- backend/src/main/java/com/yoga/backend/teacher/repository/ReservationRepository.java
+++ backend/src/main/java/com/yoga/backend/teacher/repository/ReservationRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
     List<Reservation> findByUserId(int userId);
+
     List<Reservation> findByTeacherId(int teacherId);
 }
