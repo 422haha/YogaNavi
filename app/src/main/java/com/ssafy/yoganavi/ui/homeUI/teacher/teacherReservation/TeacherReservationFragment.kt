7ad8636408eb54d7043885349257b098436753d1
정다혜 <hyejjang1203@naver.com>
2024-08-01 15:56:56 +0900

    S11P12D210-259 Feat : 실시간 강의 예약 화면 정말 멋진 애니메이션

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherReservation/TeacherReservationFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherReservation/TeacherReservationFragment.kt
@@ -2,12 +2,17 @@ package com.ssafy.yoganavi.ui.homeUI.teacher.teacherReservation
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.databinding.FragmentTeacherReservationBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class TeacherReservationFragment :
@@ -17,7 +22,7 @@ class TeacherReservationFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
-//        initListener()
+        initListener()
     }
 
     private fun initView() = with(binding) {
@@ -36,5 +41,55 @@ class TeacherReservationFragment :
             tvHashtag.text = args.hashtags
             tvHashtag.isVisible = true
         } else tvHashtag.isVisible = false
+        tvSelectClassMethod.isVisible = false
+        rgClass.isVisible = false
+        lifecycleScope.launch {
+            delay(300)
+            tvSelectClassMethod.isVisible = true
+            tvSelectClassMethod.startAnimation(
+                AnimationUtils.loadAnimation(
+                    context,
+                    R.anim.slide_up
+                )
+            )
+            rgClass.visibility = View.INVISIBLE
+            delay(500)
+            rgClass.isVisible = true
+            rbOneToMulti.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_up))
+            rbOneToOne.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_up))
+        }
+        tvAvailableClass.isVisible = false
+        lyRvAvailableClass.isVisible = false
+        tvSelectTerm.isVisible = false
+        calendar.isVisible = false
     }
+
+    private fun initListener() = with(binding) {
+        rbOneToOne.setOnClickListener {
+            visibleAvailableClass()
+        }
+        rbOneToMulti.setOnClickListener {
+            visibleAvailableClass()
+        }
+
+    }
+
+    private fun visibleAvailableClass() = with(binding) {
+        lifecycleScope.launch {
+            tvSelectTerm.isVisible = false
+            calendar.isVisible = false
+            tvAvailableClass.apply {
+                isVisible = true
+                startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_up))
+            }
+            lyRvAvailableClass.visibility = View.INVISIBLE
+            delay(500)
+            // TODO: 이거 rv 처리
+            lyRvAvailableClass.apply {
+                isVisible = true
+                startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_up))
+            }
+        }
+    }
+
 }
