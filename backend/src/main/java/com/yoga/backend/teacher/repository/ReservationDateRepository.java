256d0a0ad6171e33986dae90fa4e7b27065d574e
강미연 <422haha@naver.com>
2024-08-02 15:55:50 +0900

    S11P12D210-278 <refactor> [be]: 불필요한 예약 관련 엔티티 및 클래스 삭제

--- backend/src/main/java/com/yoga/backend/teacher/repository/ReservationDateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yoga.backend.teacher.repository;
-
-import com.yoga.backend.common.entity.ReservationDate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationDateRepository extends JpaRepository<ReservationDate, Long> {
-}
