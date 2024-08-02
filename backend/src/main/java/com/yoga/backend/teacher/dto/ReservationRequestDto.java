000196c1ac11f466a94881f2a6d84e8bb4ea3c02
강미연 <422haha@naver.com>
2024-08-02 17:12:17 +0900

    S11P12D210-280 <refactor> [be]: ReservationRequestDto 클래스 수정

--- backend/src/main/java/com/yoga/backend/teacher/dto/ReservationRequestDto.java
+++ backend/src/main/java/com/yoga/backend/teacher/dto/ReservationRequestDto.java
@@ -1,36 +1,16 @@
 package com.yoga.backend.teacher.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * 예약 요청 DTO 클래스 실시간 강의 예약 요청에 사용됩니다.
  */
+@Data
+@Builder
 public class ReservationRequestDto {
 
-    private int liveLectureId; // 실시간 강의 ID
+    private int liveId;        // 실시간 강의 ID
     private Long startDate;    // 시작 날짜 (밀리초 단위)
     private Long endDate;      // 종료 날짜 (밀리초 단위)
-
-    // getters and setters
-    public int getLiveLectureId() {
-        return liveLectureId;
-    }
-
-    public void setLiveLectureId(int liveLectureId) {
-        this.liveLectureId = liveLectureId;
-    }
-
-    public Long getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Long startDate) {
-        this.startDate = startDate;
-    }
-
-    public Long getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Long endDate) {
-        this.endDate = endDate;
-    }
 }
