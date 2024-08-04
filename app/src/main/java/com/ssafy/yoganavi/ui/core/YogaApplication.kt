cda93afd89352ac3e27087995c9ac5d9e5299c60
Aloe-droid <https://github.com/Aloe-droid>
2024-08-04 16:34:29 +0900

    S11P12D210-285 Feat: 캐시 삭제

--- app/src/main/java/com/ssafy/yoganavi/ui/core/YogaApplication.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/core/YogaApplication.kt
@@ -2,7 +2,11 @@ package com.ssafy.yoganavi.ui.core
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.File
 
 @HiltAndroidApp
 class YogaApplication : Application() {
@@ -10,5 +14,24 @@ class YogaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        CoroutineScope(Dispatchers.IO).launch { clearAllCaches() }
+    }
+
+    private fun clearAllCaches() = runCatching {
+        deleteDirectory(cacheDir)
+        externalCacheDir?.let { deleteDirectory(it) }
+        codeCacheDir?.let { deleteDirectory(it) }
+    }.onFailure {
+        it.printStackTrace()
+    }
+
+    private fun deleteDirectory(dir: File): Boolean {
+        if (!dir.exists()) return dir.delete()
+
+        dir.listFiles()?.forEach { file ->
+            if (file.isDirectory) deleteDirectory(file)
+            else file.delete()
+        }
+        return dir.delete()
     }
 }
