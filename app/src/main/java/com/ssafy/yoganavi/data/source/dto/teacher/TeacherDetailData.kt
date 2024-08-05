002feb5a5e497fb1543b0b45e7df7918e4f1e715
Aloe-droid <csjune99@naver.com>
2024-08-05 16:32:25 +0900

    S11P12D210-314 Feat: 강사 content nullable 하게 수정

--- app/src/main/java/com/ssafy/yoganavi/data/source/dto/teacher/TeacherDetailData.kt
+++ app/src/main/java/com/ssafy/yoganavi/data/source/dto/teacher/TeacherDetailData.kt
@@ -22,5 +22,5 @@ data class TeacherDetailData(
     val teacherRecorded: MutableList<LectureData> = mutableListOf(),
     @SerializedName("notices")
     val teacherNotice: MutableList<NoticeData> = mutableListOf(),
-    val content:String
+    val content: String?
 )
