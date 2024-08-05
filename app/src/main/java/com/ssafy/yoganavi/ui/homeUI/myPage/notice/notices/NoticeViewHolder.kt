1cf2242730c5c08e7aabff02703d536eea7b8c56
정다혜 <hyejjang1203@naver.com>
2024-08-05 17:22:09 +0900

    S11P12D210-312: 공지 profileImg null처리

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/notice/notices/NoticeViewHolder.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/myPage/notice/notices/NoticeViewHolder.kt
@@ -2,6 +2,7 @@ package com.ssafy.yoganavi.ui.homeUI.myPage.notice.notices
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.data.source.dto.notice.NoticeData
 import com.ssafy.yoganavi.databinding.ListItemNoticeBinding
 import com.ssafy.yoganavi.ui.utils.formatDashWeekDate
@@ -12,14 +13,17 @@ class NoticeViewHolder(
     private val navigateToRegisterNoticeFragment: (Int) -> Unit
 ) : ViewHolder(binding.root) {
     fun bind(item: NoticeData, noticeDeleteClick: (NoticeData) -> Unit) = with(binding) {
+        if (item.profileImageSmallUrl.isNullOrBlank()) {
+            binding.ivProfile.setImageResource(R.drawable.profilenull)
+        } else {
+            Glide.with(binding.root)
+                .load(item.profileImageSmallUrl)
+                .circleCrop()
+                .into(ivProfile)
 
-        Glide.with(binding.root)
-            .load(item.profileImageSmallUrl)
-            .circleCrop()
-            .into(ivProfile)
-
-        if (item.imageUrl?.isNotBlank() == true && item.imageUrlSmall?.isNotBlank() == true) {
-            ivNotice.loadImageSequentially(item.imageUrlSmall, item.imageUrl)
+            if (item.imageUrl?.isNotBlank() == true && item.imageUrlSmall?.isNotBlank() == true) {
+                ivNotice.loadImageSequentially(item.imageUrlSmall, item.imageUrl)
+            }
         }
 
         tvTeacherNickname.text = item.userName
--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherDetail/teacherDetail/TeacherDetailNoticeViewHolder.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/teacherDetail/teacherDetail/TeacherDetailNoticeViewHolder.kt
@@ -3,6 +3,7 @@ package com.ssafy.yoganavi.ui.homeUI.teacher.teacherDetail.teacherDetail
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.ssafy.yoganavi.R
 import com.ssafy.yoganavi.data.source.dto.notice.NoticeData
 import com.ssafy.yoganavi.databinding.ListItemNoticeBinding
 import com.ssafy.yoganavi.ui.utils.formatDashWeekDate
@@ -12,15 +13,18 @@ class TeacherDetailNoticeViewHolder(
     private val binding: ListItemNoticeBinding
 ) : ViewHolder(binding.root) {
     fun bind(item: NoticeData) = with(binding) {
-        Glide.with(binding.root)
-            .load(item.profileImageSmallUrl)
-            .circleCrop()
-            .into(ivProfile)
+        if (item.profileImageSmallUrl.isNullOrBlank()) {
+            binding.ivProfile.setImageResource(R.drawable.profilenull)
+        } else {
+            Glide.with(binding.root)
+                .load(item.profileImageSmallUrl)
+                .circleCrop()
+                .into(ivProfile)
 
-        if (item.imageUrl?.isNotBlank() == true && item.imageUrlSmall?.isNotBlank() == true) {
-            ivNotice.loadImageSequentially(item.imageUrlSmall, item.imageUrl)
+            if (item.imageUrl?.isNotBlank() == true && item.imageUrlSmall?.isNotBlank() == true) {
+                ivNotice.loadImageSequentially(item.imageUrlSmall, item.imageUrl)
+            }
         }
-
         tvTeacherNickname.text = item.userName
         tvDate.text = formatDashWeekDate(item.updatedAt)
         tvContent.text = item.content
