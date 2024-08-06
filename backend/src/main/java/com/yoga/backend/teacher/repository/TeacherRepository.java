4febf277767efff1f1c8ac13d5c28f4bbf3f1329
강미연 <422haha@naver.com>
2024-08-06 12:54:47 +0900

    S11P12D210-318 <fix> [be]: TeacherRepository - 강사 필터링 시 최대 인원수 제한 조건 추가

--- backend/src/main/java/com/yoga/backend/teacher/repository/TeacherRepository.java
+++ backend/src/main/java/com/yoga/backend/teacher/repository/TeacherRepository.java
@@ -74,7 +74,7 @@ public interface TeacherRepository extends JpaRepository<Users, Integer> {
         "  CASE WHEN :maxLiveNum = 1 THEN l.max_live_num > 0 ELSE l.max_live_num = 1 END" +
         ")" +
         "AND (" +
-        "  SELECT COUNT(mll) FROM MyLiveLecture mll WHERE mll.liveLecture = l AND mll.endDate > CURRENT_TIMESTAMP < l.maxLiveNum"
+        "  (SELECT COUNT(*) FROM my_live_lecture mll WHERE mll.live_id = l.live_id AND mll.end_date > CURRENT_TIMESTAMP) < l.max_live_num "
         +
         ")", nativeQuery = true)
     List<Users> findTeachersByLectureFilter(
