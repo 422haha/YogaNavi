f09b6953a5ba89526bf0955a2891a59a54ae056b
정다혜 <hyejjang1203@naver.com>
2024-08-09 15:15:09 +0900

    S11P12D210-376 Feat: 일대다 막기 및 뷰 정리

--- app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/filter/FilterFragment.kt
+++ app/src/main/java/com/ssafy/yoganavi/ui/homeUI/teacher/filter/FilterFragment.kt
@@ -14,7 +14,6 @@ import com.ssafy.yoganavi.databinding.FragmentFilterBinding
 import com.ssafy.yoganavi.ui.core.BaseFragment
 import com.ssafy.yoganavi.ui.utils.END
 import com.ssafy.yoganavi.ui.utils.FILTER
-import com.ssafy.yoganavi.ui.utils.ONE_TO_MULTI
 import com.ssafy.yoganavi.ui.utils.ONE_TO_ONE
 import com.ssafy.yoganavi.ui.utils.PERIOD_MONTH
 import com.ssafy.yoganavi.ui.utils.PERIOD_THREE_MONTH
@@ -22,7 +21,6 @@ import com.ssafy.yoganavi.ui.utils.PERIOD_TOTAL
 import com.ssafy.yoganavi.ui.utils.PERIOD_WEEK
 import com.ssafy.yoganavi.ui.utils.RECENT
 import com.ssafy.yoganavi.ui.utils.START
-import com.ssafy.yoganavi.ui.utils.TOTAL
 import com.ssafy.yoganavi.ui.utils.Week
 import com.ssafy.yoganavi.ui.utils.formatZeroDate
 import timber.log.Timber
@@ -71,7 +69,6 @@ class FilterFragment : BaseFragment<FragmentFilterBinding>(FragmentFilterBinding
                 binding.ibtnSat.isChecked = true
                 binding.ibtnSun.isChecked = true
                 binding.rbTotal.isChecked = true
-                binding.rbOneToMulti.isChecked = true
                 isInit = true
                 val directions = FilterFragmentDirections
                     .actionFilterFragmentToTeacherListFragment(viewModel.filter, isInit, sorting)
@@ -108,19 +105,6 @@ class FilterFragment : BaseFragment<FragmentFilterBinding>(FragmentFilterBinding
             PERIOD_MONTH -> binding.rbMonth.isChecked = true
             PERIOD_THREE_MONTH -> binding.rbThreeMonth.isChecked = true
             PERIOD_TOTAL -> binding.rbTotal.isChecked = true
-        }
-        when (viewModel.filter.maxLiveNum) {
-            ONE_TO_ONE -> {
-                binding.rbOneToOne.isChecked = true
-            }
-
-            ONE_TO_MULTI -> {
-                binding.rbOneToMulti.isChecked = true
-            }
-
-            else -> {
-                binding.rbTotalMethod.isChecked = true
-            }
         }
     }
 
@@ -200,13 +184,7 @@ class FilterFragment : BaseFragment<FragmentFilterBinding>(FragmentFilterBinding
             viewModel.filter.period = PERIOD_TOTAL
         }
 
-        if (binding.rbOneToOne.isChecked) {
-            viewModel.filter.maxLiveNum = ONE_TO_ONE
-        } else if (binding.rbOneToMulti.isChecked) {
-            viewModel.filter.maxLiveNum = ONE_TO_MULTI
-        } else {
-            viewModel.filter.maxLiveNum = TOTAL
-        }
+        viewModel.filter.maxLiveNum = ONE_TO_ONE
         isInit = false
         val directions = FilterFragmentDirections
             .actionFilterFragmentToTeacherListFragment(viewModel.filter, isInit, sorting)
